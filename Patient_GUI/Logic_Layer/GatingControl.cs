a7e57ac3eb627e8d2a305c8b67874ff864417b53
Martin Holmershøj <au648335@uni.au.dk>
2021-03-29 14:30:04 +0200

    Tilføjet Gating klasse

---
+++ Patient_GUI/Logic_Layer/GatingControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic_Layer
+{
+    class GatingControl
+    {
+        private bool CheckGating()
+        {
+            //Check om aktuel værdi er inden for gating området
+            return true;
+        }
+
+        private void SetGAtingWindow()
+        {
+            //Sæt gating vinduets værdier her.
+        }
+    }
+}
