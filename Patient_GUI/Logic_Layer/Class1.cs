d561892c4d7da6afad5eef93a3eda9a777805163
StarLordNumber1 <60734541+StarLordNumber1@users.noreply.github.com>
2021-03-29 10:27:26 +0200

    Subject and a list and Iobserver

--- Patient_GUI/Logic_Layer/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Logic_Layer
-{
-    public class Class1
-    {
-    }
-}
---
+++ Patient_GUI/Logic_Layer/Subject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logic_Layer
+{
+    public class Subject
+    {
+        private List<IObserver> iobserver; 
+
+
+        public interface IObserver
+        {
+            
+        }
+
+    }
+}
