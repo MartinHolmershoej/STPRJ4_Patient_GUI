704677c450114855dd7bceef6187d28848b998f0
StarLordNumber1 <60734541+StarLordNumber1@users.noreply.github.com>
2021-03-29 13:23:29 +0200

    sock1

--- Patient_GUI/Data_Layer/DataUDP.cs
+++ Patient_GUI/Data_Layer/DataUDP.cs
@@ -18,31 +18,33 @@ namespace Data_Layer
         private static int port0 = 11000;
 
 
-        private UdpClient udpClient;
-
+        
 
 
 
 
         public void UdpRecieveData()
         {
-            udpClient = new UdpClient(port0);
+            UdpClient udpClient = new UdpClient(port0);
+
+            
+            
             var remoteIP = new IPEndPoint(IPAddress.Any, port0);
             byte[] bytes;
             string jason;
 
 
-            using (Socket sock1 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0)) ;
+            using (Socket sock1 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
             {
-                sock1.connect("1213.1323.13131", 11000);
-                IPEndPoint endpoint = sock1.localEndPoint as IPEndPoint;
+                sock1.Connect("1213.1323.13131", 11000);
+                IPEndPoint endpoint = sock1.LocalEndPoint as IPEndPoint;
 
             }
             try
             {
                 while (true)
                 {
-
+                    
                 }
 
             }
