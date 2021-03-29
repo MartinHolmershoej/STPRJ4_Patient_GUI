caac65c5853d201e264e09ea9aabfee7ec0bb849
Martin Holmershøj <au648335@uni.au.dk>
2021-03-29 10:01:07 +0200

    Kode kopieret

--- Patient_GUI/Patient_GUI/MainWindow.xaml.cs
+++ Patient_GUI/Patient_GUI/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Patient_GUI
 {
@@ -26,6 +28,62 @@ namespace Patient_GUI
 
 
             // hej med dig, hvordan har du det? 
+            // godt
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+                Thread skideThread = new Thread(Skid);
+                skideThread.IsBackground = true;
+                skideThread.Start();
+            }
+
+            private void Skid()
+            {
+                int flyt = 1;
+                bool skift = true;
+                for (int j = 0; j < 1000000; j++)
+                {
+                    if (skift)
+                    {
+                        Virk_nu.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+                        {
+                            Canvas.SetBottom(FuckingVirk, flyt);
+                        }));
+                        flyt++;
+                        if (flyt == 350)
+                        {
+                            skift = false;
+                        }
+                        if (flyt >= 150 && flyt < 200)
+                        {
+                            Virk_nu.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+                            {
+                                label.Visibility = Visibility.Visible;
+                            }));
+                        }
+                        else
+                        {
+                            Virk_nu.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+                            {
+                                label.Visibility = Visibility.Hidden;
+                            }));
+                        }
+                    }
+
+                    else
+                    {
+                        Virk_nu.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+                        {
+                            Canvas.SetBottom(FuckingVirk, flyt);
+                        }));
+                        flyt--;
+                        if (flyt == 0)
+                        {
+                            skift = true;
+                        }
+                    }
+                    Thread.Sleep(10);
+                }
+            }
         }
     }
 }
