33ed27907374021397742a6ff1a286cc063df040
Martin Holmershøj <au648335@uni.au.dk>
2021-04-23 13:40:45 +0200

    Kode til UDPTheme lavet

--- Patient_GUI/Data_Layer/UDPTheme.cs
+++ Patient_GUI/Data_Layer/UDPTheme.cs
@@ -15,37 +15,33 @@ namespace Data_Layer
 
         public int RecieveTheme()
         {
-            //UdpClient udpClient = new UdpClient(port0);
-            //var remoteIP = new IPEndPoint(IPAddress.Any, port0);
-            //byte[] bytes;
-            //string jason;
+            UdpClient udpClient = new UdpClient(port0);
+            var remoteIP = new IPEndPoint(IPAddress.Any, port0);
+            byte[] bytes;
+            int _Theme =0;
 
 
-            //using (Socket sock1 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
-            //{
-            //    sock1.Connect("1213.1323.13131", 11000);
-            //    IPEndPoint endpoint = sock1.LocalEndPoint as IPEndPoint;
+            using (Socket sock2 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                sock2.Connect("1213.1323.13131", 11000);
+                IPEndPoint endpoint = sock2.LocalEndPoint as IPEndPoint;
 
-            //}
+            }
+            try
+            {
+                bytes = udpClient.Receive(ref remoteIP);
+                _Theme = Convert.ToInt32(Encoding.ASCII.GetString(bytes, 0, bytes.Length));
 
-            //try
-            //{
-            //    while (true)
-            //    {
-            //        bytes = udpClient.Receive(ref remoteIP);
-            //        jason = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
-            //    }
-
-            //}
-            //catch (SocketException e)
-            //{
-            //    Console.WriteLine(e);
-            //}
-            //finally
-            //{
-            //    udpClient.Close();
-            //}
-            return 1;
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                udpClient.Close();
+            }
+            return _Theme;
         }
 
 
